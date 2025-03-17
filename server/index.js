8f80ca42adb73dc0e5e64fe5622ed2449f4463f7
mister-nothing00 <francescodavidedivita@gmail.com>
2025-03-17 13:02:19 +0100

    adding youtube

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://spotify-clone-xz46.onrender.comò",
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
   })
